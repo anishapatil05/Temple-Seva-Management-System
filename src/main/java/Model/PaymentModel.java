721eb93aa11bf8099b5f059eaf0c1ae22619a43b
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2025-05-29 15:40:27 +0530

    Add files via upload

---
+++ src/main/java/Model/PaymentModel.java
@@ -0,0 +1,20 @@
+package com.temple;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentModel {
+
+	private Integer orderId;
+	private String name;
+	private String email;
+	private String phno;
+	private Integer amount;
+	private String orderStatus;
+	private String razorPayOrderId;
+	private Integer Person;
+}
